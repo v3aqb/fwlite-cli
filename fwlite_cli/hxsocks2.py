20b67c0d3eaec770400d50558002edff82364779
v3aqb <v3aqb@null.com>
2021-09-26 20:27:08 +0800

    update hxsocks2.py

--- fwlite_cli/hxsocks2.py
+++ fwlite_cli/hxsocks2.py
@@ -59,7 +59,7 @@ DEFAULT_METHOD = 'aes-128-cfb'
 DEFAULT_HASH = 'sha256'
 CTX = b'hxsocks2'
 MAX_STREAM_ID = 65530
-MAX_CONNECTION = 4
+MAX_CONNECTION = 2
 
 OPEN = 0
 EOF_SENT = 1   # SENT END_STREAM
@@ -412,7 +412,7 @@ class Hxs2Connection:
                     self._last_count = 0
                 self._last_count += 1
 
-                if self._last_count > 10 and random.random() < 0.01:
+                if self._last_count > 10 and random.random() < 0.1:
                     await self.send_frame(PING, PONG, 0, bytes(random.randint(64, 256)))
 
                 if frame_type == DATA:  # 0
