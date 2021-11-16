51063ab8c669cb3605bf9fec72aaf42e28e4ea3b
v3aqb <v3aqb@null.com>
2021-11-16 18:30:10 +0800

    typo

--- fwlite_cli/hxsocks2.py
+++ fwlite_cli/hxsocks2.py
@@ -681,7 +681,7 @@ class Hxs2Connection:
     def is_busy(self):
         return self._buffer_size_ewma > 2048 or \
             (self._recv_tp_max > 524288 and self._recv_tp_ewma > self._recv_tp_max * 0.3) or \
-            (self._recv_tp_max > 262144 and self._sent_tp_ewma > self._sent_tp_max * 0.3)
+            (self._sent_tp_max > 262144 and self._sent_tp_ewma > self._sent_tp_max * 0.3)
 
     def print_status(self):
         if not self.connected:
