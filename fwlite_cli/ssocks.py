d6c4958b855e4736f4bea20e870c9d23a2aac362
v3aqb <v3aqb@null.com>
2024-05-02 15:16:54 +0800

    update ssocks.py

--- fwlite_cli/ssocks.py
+++ fwlite_cli/ssocks.py
@@ -76,20 +76,20 @@ class SSConn:
 
         self._address = None
         self._port = 0
-        self.client_reader = None
-        self.client_writer = None
-        self.remote_reader = None
-        self.remote_writer = None
-        self.task = None
+        self._client_reader = None
+        self._client_writer = None
+        self._remote_reader = None
+        self._remote_writer = None
+        self._task = None
 
         self.aead = is_aead(ssmethod)
-        self.crypto = Encryptor(sspassword, ssmethod, role=0)
-        self.connected = False
-        self.last_active = time.time()
+        self._crypto = Encryptor(sspassword, ssmethod, role=0)
+        self._connected = False
+        self._last_active = time.time()
         # if eof recieved
-        self.remote_eof = False
-        self.client_eof = False
-        self.data_recved = False
+        self._remote_eof = False
+        self._client_eof = False
+        self._data_recved = False
         self._buf = b''
 
     async def connect(self, addr, port, timeout, limit, tcp_nodelay):
@@ -97,7 +97,7 @@ class SSConn:
         self._port = port
 
         from .connection import open_connection
-        self.remote_reader, self.remote_writer, _ = await open_connection(
+        self._remote_reader, self._remote_writer, _ = await open_connection(
             self.proxy.hostname,
             self.proxy.port,
             proxy=self.proxy.get_via(),
@@ -105,117 +105,122 @@ class SSConn:
             tunnel=True,
             limit=131072,
             tcp_nodelay=tcp_nodelay)
-        self.remote_writer.transport.set_write_buffer_limits(65536)
+        self._remote_writer.transport.set_write_buffer_limits(65536)
 
         # create socket_pair
         sock_a, sock_b = socket.socketpair()
         if sys.platform == 'win32':
             sock_a.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
             sock_a.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        self.client_reader, self.client_writer = await asyncio.open_connection(sock=sock_b)
-        self.client_writer.transport.set_write_buffer_limits(65536)
+        self._client_reader, self._client_writer = await asyncio.open_connection(sock=sock_b)
+        self._client_writer.transport.set_write_buffer_limits(65536)
 
         # start forward
-        self.task = asyncio.ensure_future(self.forward())
+        self._task = asyncio.ensure_future(self._forward())
 
         # return reader, writer
         reader, writer = await asyncio.open_connection(sock=sock_a, limit=limit)
         return reader, writer
 
-    async def forward(self):
+    async def create_connection(self, addr, port, transport):
+        self._address = addr
+        self._port = port
+        self._transport = transport
 
-        tasks = [asyncio.create_task(self.forward_from_client()),
-                 asyncio.create_task(self.forward_from_remote()),
+    async def _forward(self):
+
+        tasks = [asyncio.create_task(self._forward_from_client()),
+                 asyncio.create_task(self._forward_from_remote()),
                  ]
         await asyncio.wait(tasks)
-        for writer in (self.remote_writer, self.client_writer):
+        for writer in (self._remote_writer, self._client_writer):
             try:
                 writer.close()
                 await writer.wait_closed()
             except ConnectionError:
                 pass
 
-    async def forward_from_client(self):
+    async def _forward_from_client(self):
         # read from client, encrypt, sent to server
         while True:
-            fut = self.client_reader.read(self.bufsize)
+            fut = self._client_reader.read(self.bufsize)
             try:
                 data = await asyncio.wait_for(fut, timeout=6)
-                self.last_active = time.time()
+                self._last_active = time.time()
             except asyncio.TimeoutError:
                 continue
             except OSError:
-                self.remote_writer.close()
+                self._remote_writer.close()
                 break
 
             if not data:
                 break
-            if not self.connected:
+            if not self._connected:
                 header = b''.join([chr(3).encode(),
                                    chr(len(self._address)).encode('latin1'),
                                    self._address.encode(),
                                    struct.pack(b">H", self._port)])
-                if self.crypto.ctx == SS_SUBKEY_2022:
+                if self._crypto.ctx == SS_SUBKEY_2022:
                     padding_len = random.randint(0, 255)
                     header += struct.pack(b"!H", padding_len)
                     header += bytes(padding_len)
-                self.connected = True
-                self.remote_writer.write(self.crypto.encrypt(header + data))
+                self._connected = True
+                self._remote_writer.write(self._crypto.encrypt(header + data))
             else:
-                self.remote_writer.write(self.crypto.encrypt(data))
+                self._remote_writer.write(self._crypto.encrypt(data))
             try:
-                await self.remote_writer.drain()
+                await self._remote_writer.drain()
             except ConnectionError:
                 break
-        self.client_eof = True
+        self._client_eof = True
         try:
-            self.remote_writer.write_eof()
+            self._remote_writer.write_eof()
         except ConnectionError:
             pass
 
     async def _read(self):
         if self.aead:
-            fut = self.remote_reader.readexactly(18)
+            fut = self._remote_reader.readexactly(18)
             data = await asyncio.wait_for(fut, timeout=6)
-            data = self.crypto.decrypt(data)
+            data = self._crypto.decrypt(data)
             _len, = struct.unpack("!H", data)
-            fut = self.remote_reader.readexactly(_len + 16)
+            fut = self._remote_reader.readexactly(_len + 16)
             try:
                 data = await asyncio.wait_for(fut, timeout=4)
             except asyncio.TimeoutError as err:
                 raise IncompleteChunk() from err
         else:
-            fut = self.remote_reader.read(self.bufsize)
+            fut = self._remote_reader.read(self.bufsize)
             data = await asyncio.wait_for(fut, timeout=6)
-        return self.crypto.decrypt(data)
+        return self._crypto.decrypt(data)
 
-    async def forward_from_remote(self):
+    async def _forward_from_remote(self):
         # read from remote, decrypt, sent to client
 
         if self.aead:
             # read first chunk
-            if self.crypto.ctx == SS_SUBKEY_2022:
-                fut = self.remote_reader.readexactly(self.crypto.iv_len * 2 + 27)
+            if self._crypto.ctx == SS_SUBKEY_2022:
+                fut = self._remote_reader.readexactly(self._crypto.iv_len * 2 + 27)
             else:
-                fut = self.remote_reader.readexactly(self.crypto.iv_len + 18)
+                fut = self._remote_reader.readexactly(self._crypto.iv_len + 18)
             try:
                 data = await asyncio.wait_for(fut, timeout=12)
-                data = self.crypto.decrypt(data)
-                if self.crypto.ctx == SS_SUBKEY_2022:
+                data = self._crypto.decrypt(data)
+                if self._crypto.ctx == SS_SUBKEY_2022:
                     _, timestamp = struct.unpack(b'!BQ', data[:9])
                     diff = time.time() - timestamp
                     if abs(diff) > 30:
                         raise ValueError('timestamp error, diff: %.3f' % diff)
                 data_len, = struct.unpack(b'!H', data[-2:])
-                fut = self.remote_reader.readexactly(data_len + 16)
+                fut = self._remote_reader.readexactly(data_len + 16)
                 data = await asyncio.wait_for(fut, timeout=4)
-                data = self.crypto.decrypt(data)
-                self.client_writer.write(data)
+                data = self._crypto.decrypt(data)
+                self._client_writer.write(data)
             except (asyncio.TimeoutError, InvalidTag, ValueError, asyncio.IncompleteReadError) as err:
                 self.logger.error('read first chunk fail: %r', err, exc_info=False)
-                self.remote_eof = True
+                self._remote_eof = True
                 try:
-                    self.client_writer.write_eof()
+                    self._client_writer.write_eof()
                 except ConnectionError:
                     pass
                 return
@@ -223,27 +228,27 @@ class SSConn:
         while True:
             try:
                 data = await self._read()
-                self.last_active = time.time()
-                self.data_recved = True
+                self._last_active = time.time()
+                self._data_recved = True
             except asyncio.TimeoutError:
-                idle_time = time.time() - self.last_active
-                if self.client_eof and idle_time > 60:
-                    self.client_writer.close()
+                idle_time = time.time() - self._last_active
+                if self._client_eof and idle_time > 60:
+                    self._client_writer.close()
                     break
                 continue
             except (BufEmptyError, asyncio.IncompleteReadError, InvalidTag, IncompleteChunk):
-                self.client_writer.close()
+                self._client_writer.close()
                 break
 
             if not data:
                 break
             try:
-                self.client_writer.write(data)
-                await self.client_writer.drain()
+                self._client_writer.write(data)
+                await self._client_writer.drain()
             except ConnectionError:
                 break
-        self.remote_eof = True
+        self._remote_eof = True
         try:
-            self.client_writer.write_eof()
+            self._client_writer.write_eof()
         except ConnectionError:
             pass
