49177e33dbaec412f1fae41a5db46bfe946244f2
v3aqb <v3aqb@null.com>
2025-09-14 23:06:23 +0800

    disable sni rewrite

--- fwlite_cli/proxy_handler.py
+++ fwlite_cli/proxy_handler.py
@@ -757,10 +757,10 @@ class http_handler(BaseProxyHandler):
         if self.shortpath:
             if self.path.startswith(self.shortpath):
                 self.shortpath = None
-            else:
-                self.logger.info('SNI_REWRITE: path: %s, sni: %s', self.path, self.shortpath)
-                self.request_host = (self.shortpath, self.request_host[1])
-                self.shortpath = '%s:%d' % self.request_host
+            # else:
+            #     self.logger.info('SNI_REWRITE: path: %s, sni: %s', self.path, self.shortpath)
+            #     self.request_host = (self.shortpath, self.request_host[1])
+            #     self.shortpath = '%s:%d' % self.request_host
 
         # redirector
         new_url = self.conf.cic.redirect(self)
